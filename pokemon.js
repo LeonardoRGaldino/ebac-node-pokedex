5d238ade8be11b8fb39147fb3cefcadb199b51c4
Leonardo Ribeiro <leonardorgaldino@hotmail.com>
2024-07-25 20:37:38 -0300

    Criação de Schema

---
+++ pokemon.js
@@ -0,0 +1,46 @@
+const { Schema } = require ('mongoose')
+
+const Pokemon = new Schema ({
+    id: {
+        type: Number,
+        required: true,
+        },
+        nome: {
+            type: String,
+            required: true, 
+        },
+        altura:{
+            type: Number,
+            required: true,
+            min: 0,
+        },
+        peso:{
+            type: Number,
+            required: true,
+            min: 0,
+        },
+        imagem: {
+            type: String,
+            required: true,
+            validate: {
+                /*condicação de checagem: verifica se o valor foi passado*/
+                validator: (valor) => {
+                    return valor && valor.startsWith('http');
+                },
+                message: () => "a imagem deve ser uma URL absoluta"
+            }
+        },
+        ataques:{
+            type: String,
+            required: trye,
+        },
+        estatisticas: {
+            type: Object,
+            required: true
+        }
+
+        }
+
+    );
+
+module.exports = Pokemon;
